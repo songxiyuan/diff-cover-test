1e708b63b1f8275c78572facdd52199dda288966
songxiyuan <songxiyuan@didiglobal.com>
2021-10-27 19:15:45 +0800

    after

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func Print(n int) {
 	switch n {
 	case 1:
 		other_pkg.Print(1)
+	case 2:
+		other_pkg.Print(2)
+	case 3:
+		other_pkg.Print(3)
 	case 4:
 		other_pkg.Print(4)
 	case 5:
--- other_pkg/print.go
+++ other_pkg/print.go
@@ -6,6 +6,10 @@ func Print(n int) {
 	switch n {
 	case 1:
 		fmt.Println(1)
+	case 2:
+		fmt.Println(2)
+	case 3:
+		fmt.Println(3)
 	case 4:
 		fmt.Println(4)
 	case 5:
