585552d55c881d0e544deabf044ce096c9a4f01b
songxiyuan <songxiyuan@didiglobal.com>
2021-10-27 19:13:52 +0800

    before

--- main_test.go
+++ main_test.go
@@ -10,30 +10,30 @@ func Test_print(t *testing.T) {
 		name string
 		args args
 	}{
-		//{
-		//	name: "Print 1",
-		//	args: args{
-		//		n: 1,
-		//	},
-		//},
-		//{
-		//	name: "Print 4",
-		//	args: args{
-		//		n: 4,
-		//	},
-		//},
 		{
-			name: "Print 2",
+			name: "Print 1",
 			args: args{
-				n: 2,
+				n: 1,
 			},
 		},
 		{
-			name: "Print 3",
+			name: "Print 4",
 			args: args{
-				n: 3,
+				n: 4,
 			},
 		},
+		//{
+		//	name: "Print 2",
+		//	args: args{
+		//		n: 2,
+		//	},
+		//},
+		//{
+		//	name: "Print 3",
+		//	args: args{
+		//		n: 3,
+		//	},
+		//},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
